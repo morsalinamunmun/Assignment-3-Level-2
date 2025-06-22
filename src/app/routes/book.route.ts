cbb15e596b23d586bd05364d1e56d15fc98b77ae
morsalinamunmun <morsalinamunmun@gmail.com>
2025-06-22 10:05:03 +0600

    book delete & update function create

--- src/app/routes/book.route.ts
+++ src/app/routes/book.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createBook, getAllBooks, getBookById } from '../controllers/book.controller';
+import { createBook, deleteBookById, getAllBooks, getBookById, updateBookById } from '../controllers/book.controller';
 
 const router = express.Router();
 router.post('/', createBook);
 router.get('/', getAllBooks);
 router.get('/:bookId', getBookById);
+router.patch('/:bookId', updateBookById);
+router.delete('/:bookId', deleteBookById);
 
 export default router;
